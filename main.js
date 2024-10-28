207539d3203ed752161a71bf3f91c37dc7d7e0d5
Rhamudex <160138944+Rhamudex@users.noreply.github.com>
2024-10-28 08:57:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 function criaCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+  let conteiner = document.getElementById('container')
+  let article = documnet.createElement('article')
+  article.className = 'cartao'
+  article.innerHTML = 'isso é um teste'
+  conteiner.appendChild(article)
 }
+
 criaCartao('biologia','qual a funçao da mitocondria','produçao de energia')
