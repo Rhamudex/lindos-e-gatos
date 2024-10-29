0fc742ab659ee261f776cede15d7de51b389e73c
Rhamudex <160138944+Rhamudex@users.noreply.github.com>
2024-10-29 08:56:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 function criaCartao(categoria,pergunta,resposta){
   let conteiner = document.getElementById('container')
-  let article = documnet.createElement('article')
+  let article = document.createElement('article')
   article.className = 'cartao'
-  article.innerHTML = 'isso é um teste'
+  article.innerHTML = `
+ <div class="cartao-conteudo">
+  <h3> ${ categoria} </h3>
+  <div class="cartao-conteudo-pergunta"> 
+  <p> ${pergunta} </p>
+  </div>
+  <div class="cartao-conteudo-resposta">
+    <p> ${resposta}</p>
+  </div>
+  </div>
+  `
   conteiner.appendChild(article)
 }
 
-criaCartao('biologia','qual a funçao da mitocondria','produçao de energia')
+criaCartao('Biologia','Qual a funçao da mitocondria?','Produçao de energia')
+criaCartao('Strapazzon', 'Qual nome do namorado da Strapazzon?', 'Joao Rafael')
